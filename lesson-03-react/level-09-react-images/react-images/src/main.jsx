9157602a88af628320c51044f93c1e41f2de6604
Jennifer Tarleton <jennifertarleton@gmail.com>
2026-06-25 15:14:49 -0500

    refactor main.jsx

--- lesson-03-react/level-09-react-images/react-images/src/main.jsx
+++ lesson-03-react/level-09-react-images/react-images/src/main.jsx
@@ -3,7 +3,10 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 
-createRoot(document.getElementById('root')).render(
+const divTag =  document.getElementById('root');
+
+createRoot(divTag)
+.render(
   <StrictMode>
     <App />
   </StrictMode>,
