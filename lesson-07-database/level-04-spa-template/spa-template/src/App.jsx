0d4473ee0f0b07b827afcc17aac18803304427dd
Jennifer Tarleton <jennifertarleton@gmail.com>
2026-07-17 01:13:16 -0500

    Added new page to level 4 lesson 7

--- lesson-07-database/level-04-spa-template/spa-template/src/App.jsx
+++ lesson-07-database/level-04-spa-template/spa-template/src/App.jsx
@@ -4,6 +4,7 @@ import Footer from "./components/Footer.jsx";
 import "./App.css";
 import { BrowserRouter, Routes, Route } from "react-router";
 import ApiKey from "./pages/ApiKey.jsx";
+import Pokemon from "./pages/Pokemon.jsx";
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/apikey" element={<ApiKey />} />
+        <Route path="/pokemon" element={ <Pokemon/>}/>
       </Routes>
 
       <Footer />
--- lesson-07-database/level-04-spa-template/spa-template/src/components/NavBar.jsx
+++ lesson-07-database/level-04-spa-template/spa-template/src/components/NavBar.jsx
@@ -81,18 +81,18 @@ export default function NavBar() {
                 </NavLink>
               </div>
               {/* <!-- Pricing link --> */}
-              {/* <div
+              <div
                 className="mb-4 ps-2 lg:mb-0 lg:pe-1 lg:ps-0"
                 data-twe-nav-item-ref
               >
                 <NavLink
                   className="p-0 text-black/60 transition duration-200 hover:text-black/80 hover:ease-in-out focus:text-black/80 active:text-black/80 motion-reduce:transition-none dark:text-white/60 dark:hover:text-white/80 dark:focus:text-white/80 dark:active:text-white/80 lg:px-2"
-                  to="/"
+                  to="/pokemon"
                   data-twe-nav-link-ref
                 >
-                  Pricing
+                  Pokemon
                 </NavLink>
-              </div> */}
+              </div>
               
             </div>
           </div>
